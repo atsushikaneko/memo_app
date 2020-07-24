34170c4d1e50f877aae4aa7ffbeb73e5e634dc90
兼子敦史 <atsushi@kanekoatsushinoMacBook-Air.local>
2020-07-24 17:46:51 +0900

    add: ルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'memo/index'
+  root to: "memo#index"
 
-  get 'memo/create'
-
-  get 'memo/detroy'
-
-  get 'memo/category'
+  resources :memo do
+    collection do
+      get 'category'
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
